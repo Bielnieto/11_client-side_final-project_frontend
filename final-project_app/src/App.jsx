7c002140262ee460eaa18c2d31a60e7b3984cf0a
Biel <biel.nieto@mataro.epiaedu.cat>
2025-05-15 19:51:34 +0200

    Correct API value, and useEffect hook used for check the API value.

--- final-project_app/src/App.jsx
+++ final-project_app/src/App.jsx
@@ -1,9 +1,15 @@
 
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  useEffect(() => {
+    console.log(import.meta.env.VITE_API_URL)
+  },[])
+
   return (
+    
     <>
       
     </>
